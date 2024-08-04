f4f184b4668d8275fdfc120bbc6cbbcac04ee641
Shauray Dhingra <shauraydhingra03@gmail.com>
2024-08-04 16:44:57 +0530

    some how last build failed

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client';
-
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
